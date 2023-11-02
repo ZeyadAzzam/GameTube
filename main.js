e902b2c6e139b345f1183acd19f892150ab90c20
scrim666 <zoz341995@gmail.com>
2023-11-02 12:40:18 +0300

    edit some styles and api

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
- const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '790e135616msh78bc98c720bad91p1fde9bjsn6227394df532',
-    'X-RapidAPI-Host': 'videogames-news2.p.rapidapi.com'
-  }
-};
-//async function 
-async function fetchTopNews() {
+// async function to fetch game news
+async function fetchGameNews() {
   try {
-    const response = await fetch('https://videogames-news2.p.rapidapi.com/videogames_news/recent', options);
+    const apiKey = "02f2dac72abf49419c6d196a9ebef749";
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=video%20games&apiKey=${apiKey}`
+    );
     const data = await response.json();
-    
-    data.forEach(News => {
-      // list create contain the news
-      const news = `<li> 
-      <a  href="${News.link}" class ="news-link" target="_blank">
-        <img src="${News.image}" alt="News Image" class= "news-img" title = "more details"></a>
-        <br>
-      <b class="news-title">${News.title} </b> <br>
-     <div class="news-descrption"> ${News.description}</div>
-    </li>`;
 
-    // the function to display the list on HTMl page 
-    document.querySelector('.news-list').insertAdjacentHTML("beforebegin",news);
+    const newsList = document.querySelector(".news-list");
+
+    data.articles.forEach((article) => {
+      const news = `
+        <li>
+          <a href="${article.url}" class="news-link" target="_blank">
+            <img src="${article.urlToImage}" alt="News Image" class="news-img" title="more details">
+          </a>
+          <br>
+          <b class="news-title">${article.title}</b>
+          <br>
+          <div class="news-description">${article.description}</div>
+        </li>
+      `;
+      newsList.innerHTML += news;
     });
-    
-  }
-  // handle any error when we fetch the API
-  catch (error) {
-    console.error('Error fetching top news:', error);
+  } catch (error) {
+    console.error("Error fetching game news:", error);
   }
 }
 
-// call the function fetchNews
-fetchTopNews();
+// Call the function to fetch game news
+fetchGameNews();
 
-// Event lister to Search button and 
-document.getElementById("search-form").addEventListener("submit", function() {
+// Event lister to Search button and
+document.getElementById("search-form").addEventListener("submit", function () {
   const searchInput = document.getElementById("search-input").value;
 
   const specialCharacters = /[!@#$%]/;
@@ -44,16 +40,20 @@ document.getElementById("search-form").addEventListener("submit", function() {
     alert("Invalid input. Special characters are not allowed.");
     return;
   }
-  
+
   // the url that contain the html file name and the Search from user
-  const userUrl = "Steam.html?search=" +(searchInput);
-  // Open new tab 
+  const userUrl = "Steam.html?search=" + searchInput;
+  // Open new tab
   window.open(userUrl, "_blank");
 });
 
-// When The user click in a contact us nav link the website go to the div that contain social media 
+// When The user click in a contact us nav link the website go to the div that contain social media
 
-document.getElementById('social-links').addEventListener('click', function(event) {
-event.preventDefault(); 
-document.getElementById('social-media').scrollIntoView({ behavior: 'smooth' });
-});
+document
+  .getElementById("social-links")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .getElementById("social-media")
+      .scrollIntoView({ behavior: "smooth" });
+  });
